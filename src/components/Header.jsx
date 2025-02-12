55ef52583477887e0fa416a16d1da551b5f889d3
Mohammed-Adel56 <168343516+Mohammed-Adel56@users.noreply.github.com>
2025-02-12 16:50:03 +0200

    update final 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -107,8 +107,8 @@ function Header() {
                 alt="Search" 
                 className="absolute top-0 right-[-5px] w-[74px] h-full" 
               />
-              <div className="absolute top-1 right-30 text-gray-400 text-sm text-right">
-                <input value="...البحث"/>
+              <div className="absolute top-1 right-0 text-gray-400 text-sm text-right">
+                <input placeholder="...البحث"/>
               </div>
             </div>
           </div>
@@ -163,8 +163,8 @@ function Header() {
               alt="Search" 
               className="absolute top-0 right-[-5px] w-[74px] h-full" 
             />
-            <div className="absolute top-1 right-20 text-gray-400 text-sm text-right">
-            <input value="...البحث"/>
+            <div className="absolute top-1 right-0 text-gray-400 text-sm text-right">
+            <input placeholder="...البحث"/>
             </div>
           </div>
         </div>
